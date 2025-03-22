102bd192bd088fafec4befcf95de4da918c2840c
Isaac-Balderas <raul.balderas.ga@gmail.com>
2025-03-21 18:43:08 -0600

    Server side components

--- app/dashboard/_components/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { LuStore } from "react-icons/lu";
-
-export default function Sidebar(){
-    return (
-        <nav className="w-[10vw] h-[90vh] bg-orange-200 flex flex-col items-center py-20">
-            <LuStore className="text-4xl" />
-        </nav>
-    );
-}
---
+++ app/dashboard/_components/_sidebar/NavItem.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+interface NavItemProps{
+    icon: ReactNode
+    path: string
+}
+
+const NavItem = ({ icon, path }: NavItemProps) =>{
+    const pathName = usePathname();
+    return (
+        <Link href={path} className={pathName == path ? "bg-orange-400 w-full flex justify-center transition-colors" : ""}>
+        {icon}
+        </Link>
+    )
+}
+
+export default NavItem;
